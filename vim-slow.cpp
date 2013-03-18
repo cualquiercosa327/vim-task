112d8f9e2e1edeea389d17bebf3fad1723bd2ca9
Simon Lindholm <lindholm@spotify.com>
2013-03-18 14:13:13 +0100

    initial commit

---
+++ vim-slow.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <string>
+using namespace std;
+
+const int INF = 1 << 29;
+const int ALPHA = 28, E = 4;
+
+string line;
+int N;
+
+int eend;
+vector<int> pos, invpos;
+vector<vector<int> > dist;
+vector<vector<int> > directdist;
+
+int ctoi(char c) {
+	if ('a' <= c && c <= 'z')
+		return c - 'a';
+	if (c == ' ')
+		return 26;
+	return 27;
+}
+
+void finddists(int efrom) {
+	int from = pos[efrom] + 1;
+	vector<int> directdist(N, INF);
+	vector<int> active(ALPHA, 2);
+	for (int pos = from + 1; pos < N; ++pos) {
+		int c = ctoi(line[pos]);
+		if (c == E)
+			continue;
+		directdist[pos] = active[c];
+		active[c] += 2;
+		for (int j = 0; j < ALPHA; ++j) {
+			if (j == E)
+				continue;
+			active[j] = min(active[j], active[c]);
+		}
+	}
+	int cur = INF;
+	for (int pos = N; pos --> from+1; ) {
+		++cur; // "h"
+		cur = min(cur, directdist[pos]);
+		if (line[pos] == 'e')
+			dist[efrom][invpos[pos]] = cur;
+	}
+}
+
+int solve(int eat, int efirstUncovered) {
+	if (efirstUncovered == eend)
+		return 0;
+	int ret = INF;
+	for (int i = efirstUncovered; i < eend; ++i) {
+		ret = min(ret, dist[eat][i] + pos[i] - pos[efirstUncovered] + solve(efirstUncovered, i+1));
+	}
+	return ret;
+}
+
+int main() {
+	int res = 0;
+	getline(cin, line);
+	while (line[res] == 'e')
+		++res;
+	line = line.substr(res);
+	line = "e" + line;
+	N = (int)line.size();
+
+	invpos.resize(N);
+	for (int i = 0; i < (int)line.size(); ++i) {
+		if (line[i] == 'e') {
+			invpos[i] = (int)pos.size();
+			pos.push_back(i);
+		}
+	}
+	eend = (int)pos.size();
+	dist.assign(eend, vector<int>(eend, INF));
+
+	for (int i = 0; i < eend; ++i) {
+		finddists(i);
+	}
+
+	res += solve(0, 1);
+	res += eend - 1; // all the "x", except for the fake one
+	cout << res << endl;
+	return 0;
+}
---
+++ vim.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <string>
+using namespace std;
+
+typedef unsigned char Alpha;
+static const Alpha ALPHA = 28;
+static const Alpha E = Alpha('e' - 'a');
+Alpha charToAlpha(char c) {
+	if ('a' <= c && c <= 'z')
+		return Alpha(c - 'a');
+	if (c == ' ')
+		return 26;
+	if (c == '.')
+		return 27;
+	assert(false);
+}
+
+static vector<Alpha> ar;
+static int N, lastE = -1;
+
+int solveRightSingleBack(int pos, Alpha pass);
+int solveRight(int pos, Alpha pass);
+int solve(int pos, Alpha pass1, Alpha pass2);
+
+bool anyEAfter(int pos) {
+	return pos > lastE;
+}
+
+vector<int> cache1, cache2, cache3;
+int mhash(int pos, Alpha pass1) { return pos * ALPHA + pass1; }
+int mhash(int pos, Alpha pass1, Alpha pass2) { return mhash(pos, pass1) * ALPHA + pass2; }
+
+// includes back
+int solveRightSingleBack(int pos, Alpha pass) {
+	assert(pass != E);
+	if (pos == N)
+		return 1 << 29;
+	int& mem = cache3[mhash(pos, pass)];
+	if (mem >= 0) return mem;
+
+	if (ar[pos] != pass)
+		return mem = solveRightSingleBack(pos+1, pass) + 1; // "h"
+
+	// If we have all E's, we must go back. Else go on.
+	if (anyEAfter(pos))
+		return mem = 1; // "h"
+
+	int res = 1 << 29;
+	for (Alpha c = 0; c < ALPHA; ++c) {
+		if (c == E)
+			continue;
+		res = min(res, solveRightSingleBack(pos+1, c) + 3); // "fc", "h"
+	}
+	return mem = res;
+}
+
+// no back
+int solveRight(int pos, Alpha pass) {
+	assert(pass != E);
+	if (pos == N)
+		return 1 << 29;
+	int& mem = cache2[mhash(pos, pass)];
+	if (mem >= 0) return mem;
+
+	if (ar[pos] == E) {
+		int base = 0, npos = pos;
+		while (ar[npos] == E) {
+			++npos;
+			++base;
+		}
+		--base; // last e is accounted for by called functions
+
+		int res = 1 << 29;
+		for (Alpha c = 0; c < ALPHA; ++c) {
+			if (c == E)
+				continue;
+			// We'd want to say |res = min(res, solve(npos, pass, c) + 2)| here, but
+			// it doesn't work because then c can hit ar[npos]. Simulate solve()
+			// without that check instead.
+			if (ar[npos] != pass) {
+				// the "c" pressed starts with npos + 1
+				res = min(res, solve(npos+1, pass, c) + 3); // "h", "fc"
+			}
+			else {
+				res = min(res, solveRight(npos+1, c) + 3); // "h", "fc"
+				for (Alpha npass = 0; npass < ALPHA; ++npass) {
+					if (npass == E)
+						continue;
+					res = min(res, solve(npos+1, npass, c) + 5); // "fc", "h", "fc"
+				}
+			}
+		}
+		res = min(res, solveRightSingleBack(npos, pass));
+		return mem = res + base; // also 'base' many "h"
+	}
+	else if (ar[pos] == pass) {
+		int res = 1 << 29;
+		for (Alpha c = 0; c < ALPHA; ++c) {
+			if (c == E)
+				continue;
+			res = min(res, solveRight(pos+1, c) + 2); // "fc"
+		}
+		return mem = res;
+	}
+	else {
+		return mem = solveRight(pos+1, pass);
+	}
+}
+
+// includes back
+int solve(int pos, Alpha pass1, Alpha pass2) {
+	if (pos == N)
+		return 1 << 29;
+	int& mem = cache1[mhash(pos, pass1, pass2)];
+	if (mem >= 0) return mem;
+
+	if (ar[pos] != pass1) {
+		if (ar[pos] != pass2) {
+			return mem = solve(pos+1, pass1, pass2) + 1; // "h"
+		}
+		else {
+			int res = 1 << 29;
+			for (Alpha c = 0; c < ALPHA; ++c) {
+				if (c == E)
+					continue;
+				res = min(res, solve(pos+1, pass1, c) + 3); // "h", "fc"
+			}
+			return mem = res;
+		}
+	}
+	else {
+		// Either step back...
+		int res = 1 + solveRight(pos, pass2);
+
+		// ... or continue.
+		for (Alpha c1 = 0; c1 < ALPHA; ++c1) {
+			if (c1 == E)
+				continue;
+
+			if (ar[pos] != pass2) {
+				res = min(res, solve(pos+1, c1, pass2) + 3); // "fc", "h"
+			}
+			else {
+				for (Alpha c2 = 0; c2 < ALPHA; ++c2) {
+					if (c2 == E)
+						continue;
+					res = min(res, solve(pos+1, c1, c2) + 5); // "fc", "h", "fc"
+				}
+			}
+		}
+		return mem = res;
+	}
+}
+
+int main() {
+	string line;
+	getline(cin, line);
+	N = (int)line.size();
+	ar.resize(N);
+	for (int i = 0; i < N; ++i)
+		ar[i] = charToAlpha(line[i]);
+
+	cache1.assign(N * ALPHA * ALPHA, -1);
+	cache2.assign(N * ALPHA, -1);
+	cache3.assign(N * ALPHA, -1);
+
+	int res = 0;
+	for (int i = 0; i < N; ++i) {
+		if (ar[i] == E) {
+			++res; // "x"
+			lastE = i;
+		}
+	}
+	assert(lastE != N-1);
+
+	int start = 0;
+	while (ar[start] == E)
+		++start;
+
+	if (start <= lastE)
+		res += solveRight(start, charToAlpha(line[start]));
+
+	cout << res << endl;
+	return 0;
+}
